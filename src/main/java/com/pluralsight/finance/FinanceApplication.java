e59dbd6dcc99e9ff61e5ccb5fa35c8a663135d5f
mariannaaspuria <aspuria.mp@gmail.com>
2025-11-04 14:48:12 -0600

    fincance application update commit

--- src/main/java/com/pluralsight/finance/FinanceApplication.java
+++ src/main/java/com/pluralsight/finance/FinanceApplication.java
@@ -1,4 +1,25 @@
 package com.pluralsight.finance;
 
 public class FinanceApplication {
+
+    public static void main(String[] args) {
+
+        System.out.println("===== List of Portfolio's =====");
+
+        Portfolio portfolio1 = new Portfolio("Harry Potter's Net worth","Harry Potter");
+        portfolio1.add(new CheckingAccount("Checking", "4444", 500_000));
+        portfolio1.add(new CreditCard("Credit Card", "3333", 4000));
+        portfolio1.add(new Gold("Gold Coins", 200, 5));
+        portfolio1.add(new House("Vacay house", 100_000, 2025, 3000, 20));
+        portfolio1.add(new Jewelry("Necklace", 3000, 5));
+
+        System.out.println(portfolio1);
+
+        Portfolio portfolio2 = new Portfolio("Hello Kitty's Net worth","Hello Kitty");
+        portfolio2.add(new House("Family House", 600_000, 1890, 5000, 40));
+
+        System.out.println(portfolio2);
+    }
+
+
 }
