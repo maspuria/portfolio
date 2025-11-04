fbcb3061ec5613c4c9c9e00133cff121c58eea6f
mariannaaspuria <aspuria.mp@gmail.com>
2025-11-04 14:47:22 -0600

    account, credit card commit

--- src/main/java/com/pluralsight/finance/Account.java
+++ src/main/java/com/pluralsight/finance/Account.java
@@ -2,8 +2,8 @@ package com.pluralsight.finance;
 
 public abstract class Account implements Valuable {
     //field
-    private String name;
-    private String accountNumber;
+    private final String name;
+    private final String accountNumber;
     private double balance;
 
     //constructor
@@ -14,21 +14,21 @@ public abstract class Account implements Valuable {
     }
 
     //getter and setter
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAccountNumber() {
-        return accountNumber;
-    }
-
-    public void setAccountNumber(String accountNumber) {
-        this.accountNumber = accountNumber;
-    }
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getAccountNumber() {
+//        return accountNumber;
+//    }
+//
+//    public void setAccountNumber(String accountNumber) {
+//        this.accountNumber = accountNumber;
+//    }
 
     public double getBalance() {
         return balance;
